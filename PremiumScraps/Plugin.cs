b1eeb1f29624e5bdd0a7ad7f9bae1df0ac5bb7a7
ZigzagAwaka <joledoux18@gmail.com>
2025-06-02 21:48:06 +0200

    Added Matty Fixes as a soft dependency

--- PremiumScraps/Plugin.cs
+++ PremiumScraps/Plugin.cs
@@ -20,6 +20,7 @@ namespace PremiumScraps
     [BepInDependency(LethalThings.Plugin.ModGUID, BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("Theronguard.EmergencyDice", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("Zaggy1024.OpenBodyCams", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency("mattymatty.MattyFixes", BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
         const string GUID = "zigzag.premiumscraps";
